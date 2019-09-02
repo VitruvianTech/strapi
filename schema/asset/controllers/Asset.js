d1313e21dc6895595b83583b0bb00ddbb91510f9
Peter C. Romano <pete@vitruvian.tech>
2019-09-02 13:46:50 -0400

    Finish Post v1 schema

---
+++ schema/asset/controllers/Asset.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/**
+ * Asset.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Asset`.
+ */
+
+module.exports = {
+
+  /**
+   * Retrieve asset records.
+   *
+   * @return {Object|Array}
+   */
+
+  find: async (ctx, next, { populate } = {}) => {
+    if (ctx.query._q) {
+      return strapi.services.asset.search(ctx.query);
+    } else {
+      return strapi.services.asset.fetchAll(ctx.query, populate);
+    }
+  },
+
+  /**
+   * Retrieve a asset record.
+   *
+   * @return {Object}
+   */
+
+  findOne: async (ctx) => {
+    if (!ctx.params._id.match(/^[0-9a-fA-F]{24}$/)) {
+      return ctx.notFound();
+    }
+
+    return strapi.services.asset.fetch(ctx.params);
+  },
+
+  /**
+   * Count asset records.
+   *
+   * @return {Number}
+   */
+
+  count: async (ctx) => {
+    return strapi.services.asset.count(ctx.query);
+  },
+
+  /**
+   * Create a/an asset record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    return strapi.services.asset.add(ctx.request.body);
+  },
+
+  /**
+   * Update a/an asset record.
+   *
+   * @return {Object}
+   */
+
+  update: async (ctx, next) => {
+    return strapi.services.asset.edit(ctx.params, ctx.request.body) ;
+  },
+
+  /**
+   * Destroy a/an asset record.
+   *
+   * @return {Object}
+   */
+
+  destroy: async (ctx, next) => {
+    return strapi.services.asset.remove(ctx.params);
+  }
+};
